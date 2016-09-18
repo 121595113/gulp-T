bd8430ef1a0dcc4b4277b6c2b2c3c058461a009a
于涛 <121595113@qq.com>
2016-09-18 10:04:04 +0800

    Revert "疑似解决兼容window下的error处理"
    This reverts commit 626984db6ea8f76db1342e2fb74c1bdd5c1a0e8a.

--- gulp/tasks/development/delete.js
+++ gulp/tasks/development/delete.js
@@ -1,9 +1,9 @@
 import gulp from 'gulp';
 import del from 'del';
 const project = require('../../lib/project')();
-const config = require('../../config' + project).delete;
+const config = require('../../config'+project).delete;
 
 /**
  * Delete folders and files
  */
-gulp.task('delete', () => del.bind(null, config.src));
+gulp.task('delete', del.bind(null, config.src));
