8e044ab75ffe4197e73f6f556dd1fee580c5bc96
121595113@qq.com <121595113@qq.com>
2016-09-19 22:25:57 +0800

    配置sass生产模式

--- gulp/tasks/development/sass.js
+++ gulp/tasks/development/sass.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
 import plumber from 'gulp-plumber';
+import cleancss from 'gulp-clean-css';
 import sourcemaps from 'gulp-sourcemaps';
 import autoprefixer from 'gulp-autoprefixer';
 const handleErrors = require('../../lib/handleErrors');
@@ -9,6 +10,17 @@ gulp.task('sass', () => {
     return gulp.src('app/_source/sass/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', handleErrors))
+        .pipe(autoprefixer({ browsers: ['> 1% in CN', 'last 10 versions', 'Firefox ESR'] }))
         .pipe(sourcemaps.write('./.map'))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('sass:build', () => {
+    return gulp.src('app/_source/sass/**/*.scss')
+        .pipe(sass().on('error', handleErrors))
+        .pipe(autoprefixer({ browsers: ['> 1% in CN', 'last 10 versions', 'Firefox ESR'] }))
+        .pipe(cleancss({
+            compatibility: 'ie8'
+        }))
         .pipe(gulp.dest('dist/css'));
 });
