3586953b1a13505c3fff1c62815b2141df8c997c
于涛 <121595113@qq.com>
2016-10-11 13:09:58 +0800

    修改正则匹配规则

--- gulp/lib/project.js
+++ gulp/lib/project.js
@@ -1,7 +1,7 @@
 module.exports = function() {
     let args = '';
     process.argv.slice(2).forEach(function(item, index) {
-        let arg = (arg = item.match(/^\-{2}(.*)$/)) && arg[1];
+        let arg = (arg = item.match(/^-{2}(.*)$/)) && arg[1];
 
         if (arg) {
             args = '.' + arg
