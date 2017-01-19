99a2811d05d48a05dc08e3ebf83269ba0b2c6030
于涛 <121595113@qq.com>
2017-01-19 18:08:37 +0800

    添加retina合图方法

--- gulp/tasks/development/sprites.js
+++ gulp/tasks/development/sprites.js
@@ -21,21 +21,52 @@ const sprite_arg = require('../../lib/sprite_arg')();
 gulp.task('sprites', () => {
     const imgStreams = {};
     if (sprite_arg['dir_names']) {
-        sprite_arg['dir_names'].forEach(function(item, index) {
-            imgStreams[item] = gulp.src(`${config.src}/${item}/*.png`)
-                .pipe($.plumber())
-                .pipe($.spritesmith({
-                    cssName: `_${item}.scss`,
-                    cssFormat: 'scss',
-                    padding: 4,
-                    algorithm: sprite_arg.layout,
-                    cssOpts: {
-                        dir_name: item
-                    },
-                    cssTemplate: path.resolve('./gulp/lib/handlebarsInheritance.scss.handlebars'),
-                    imgName: `${item}-sprite.png`,
-                    imgPath: `../images/${item}-sprite.png?${new Date().valueOf()}`
-                }));
+        sprite_arg['dir_names'].forEach((item, index) => {
+
+            const files = fs.readdirSync(`${config.src}/${item}`);
+            let retina = null;
+
+            for (let i = 0;i < files.length;i++) {
+               if (retina) break;
+               if (/@2x\.png$/i.test(files[i])){
+                retina = true;
+               }
+            }
+
+            if (retina){
+                imgStreams[item] = gulp.src(`${config.src}/${item}/*.png`)
+                    .pipe($.plumber())
+                    .pipe($.spritesmith({
+                        retinaSrcFilter: `${config.src}/${item}/*@2x.png`,
+                        cssName: `_${item}.scss`,
+                        cssFormat: 'scss',
+                        padding: 4,
+                        algorithm: sprite_arg.layout,
+                        cssOpts: {
+                            dir_name: item,
+                            retinaImgPath: `../images/${item}-sprite@2x.png?${new Date().valueOf()}`
+                        },
+                        cssTemplate: path.resolve('./gulp/lib/handlebarsInheritance.scss.handlebars'),
+                        imgName: `${item}-sprite.png`,
+                        retinaImgName: `${item}-sprite@2x.png`,
+                        imgPath: `../images/${item}-sprite.png?${new Date().valueOf()}`
+                    }));
+            }else{
+                imgStreams[item] = gulp.src(`${config.src}/${item}/*.png`)
+                    .pipe($.plumber())
+                    .pipe($.spritesmith({
+                        cssName: `_${item}.scss`,
+                        cssFormat: 'scss',
+                        padding: 4,
+                        algorithm: sprite_arg.layout,
+                        cssOpts: {
+                            dir_name: item
+                        },
+                        cssTemplate: path.resolve('./gulp/lib/handlebarsInheritance.scss.handlebars'),
+                        imgName: `${item}-sprite.png`,
+                        imgPath: `../images/${item}-sprite.png?${new Date().valueOf()}`,
+                    }));
+            }
 
             imgStreams[item].img
                 .pipe(gulp.dest(config.dest.image));
