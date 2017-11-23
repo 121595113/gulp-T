10a26f9ae5ca041199b1c1bf6819961ca2d7321d
yutao <121595113@qq.com>
2017-11-23 17:33:58 +0800

    记一jQuery用法

--- build/app/js/index.js
+++ build/app/js/index.js
@@ -1,2 +1,2 @@
-"use strict";$(function(){$.ajax("/api").then(function(n){console.log(n)}).fail(function(n){console.error(n)})});
+"use strict";$(function(){$.ajax("/images/cur-sprite.png").then(function(n){return console.log(1111),$.ajax("/images/cur1-sprite.png")}).then(function(n){console.log(222)}).fail(function(n){console.error(n)})});
 //# sourceMappingURL=.map/index.js.map
--- src/app/js/index.js
+++ src/app/js/index.js
@@ -1,7 +1,11 @@
 $(function () {
-  $.ajax('/api').then(function (data) {
-    console.log(data);
-  }).fail(function (e) {
-    console.error(e)
-  })
+  $.ajax('/images/cur-sprite.png').then(function (data) {
+      console.log(1111);
+      return $.ajax('/images/cur1-sprite.png');
+    }).then(function (data) {
+      console.log(222);
+    })
+    .fail(function (e) {
+      console.error(e);
+    });
 });
