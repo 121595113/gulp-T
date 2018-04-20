0005774d28237b4e5b9e126f69eb396607efccfc
yutao <121595113@qq.com>
2018-04-20 11:35:29 +0800

    改变文件监听方式

--- gulp/tasks/sub-tasks/watch.js
+++ gulp/tasks/sub-tasks/watch.js
@@ -8,10 +8,10 @@ gulp.task('watch', () => {
   let { tasks, _tasks } = config;
   for (const task of tasks) {
     if (!_tasks[task]) continue;
-    if(task !== 'imagemin'){
-      gulp.watch(_tasks[task], [task]);
-      continue;
-    }
+    // if(task !== 'imagemin'){
+    //   gulp.watch(_tasks[task], [task]);
+    //   continue;
+    // }
     watch(_tasks[task], () => {
       gulpSequence(task)();
     });
